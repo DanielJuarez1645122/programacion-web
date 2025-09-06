2ec057ee582fe00414219f322e8ca66db56e16a1
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-09-06 14:06:22 -0600

    feat(libro): creé el modelo del libro con fk

--- core/models.py
+++ core/models.py
@@ -6,3 +6,11 @@ class Autor(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    fecha_publicacion = models.DateField()
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
