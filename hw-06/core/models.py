7485c99fd89438efcbe6a1955b8e0d0fe9e4258c
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-06 20:17:27 -0600

    chore(init): inicie y copie el proyecto de la tarea anterior

---
+++ hw-06/core/models.py
@@ -0,0 +1,32 @@
+from django.db import models # type: ignore
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    fecha_publicacion = models.DateField()
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.CharField(max_length=50)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Resena(models.Model):
+    texto = models.TextField()
+    calificacion = models.IntegerField()
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Resena {self.id}"
