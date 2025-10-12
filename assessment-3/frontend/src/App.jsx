ee17a07c35b2dfeaea16fd22dfffb6c4a0a56a63
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2025-10-12 12:36:45 -0600

    style(app): Le agregue estilo a la aplicacion

--- assessment-3/frontend/src/App.jsx
+++ assessment-3/frontend/src/App.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import HideSecret from './components/hide_secret';
 import RevealSecret from './components/reveal_secret';
+import "./styles/app.css";
 
 export default function App() {
   const [tab, setTab] = useState('ocultar');
 
   return (
-    <div>
-      <button onClick={() => setTab('ocultar')}>Ocultar</button>
-      <button onClick={() => setTab('revelar')}>Revelar</button>
-      <hr />
-      {tab === 'ocultar' ? <HideSecret /> : <RevealSecret />}
+    <div className="app-container">
+      <div className="tab-container">
+        <button
+          className={`tab-button ${tab === 'ocultar' ? 'active' : ''}`}
+          onClick={() => setTab('ocultar')}
+        >
+          Ocultar
+        </button>
+        <button
+          className={`tab-button ${tab === 'revelar' ? 'active' : ''}`}
+          onClick={() => setTab('revelar')}
+        >
+          Revelar
+        </button>
+      </div>
+
+      <div className="content-box">
+        {tab === 'ocultar' ? <HideSecret /> : <RevealSecret />}
+      </div>
     </div>
   );
 }
+
